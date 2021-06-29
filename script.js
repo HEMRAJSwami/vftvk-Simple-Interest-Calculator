e3900c7b74e0c262968a06404763658cd1d039c7
Ramesh Sannareddy <rsannareddy@gmail.com>
2021-06-29 22:32:26 +0000

    Hemraj complete it

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function compute()
     var interest = principal * years * rate /100;
     var amount=principal+interest;
     var year = new Date().getFullYear()+parseInt(years);
+    var doc = document.getElementById("result");
+    doc.innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"; 
     if(principal<=0)
-        alert("Enter a positive number");
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";    
+        alert("Enter a positive number");   
 }
 function updateRate() 
     {
